ae95f35683f6d20beb5befda22fda58824b876d9
Omar Nassar <omar.portero.nassar1@gmail.com>
2024-12-07 21:25:08 +0100

    No message

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://127.0.0.1:8000/dashboard",
+  baseURL: "https://app.quotum.cloud/dashboard",
   headers: {
     "Content-Type": "application/json",
   },
