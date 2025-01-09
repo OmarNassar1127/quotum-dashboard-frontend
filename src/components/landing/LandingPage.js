fe1940acee3ec2b0e4e459c27c829de8231709b1
Omar Nassar <omar.portero.nassar1@gmail.com>
2025-01-09 21:05:46 +0100

    more changes

--- src/components/landing/LandingPage.js
+++ src/components/landing/LandingPage.js
@@ -479,7 +479,6 @@ const LandingPage = () => {
       </section>
 
       {/* Footer */}
-      {/* Footer */}
       <footer className="bg-[#111] text-gray-400 py-12">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           {/* Main Footer Content */}
@@ -502,8 +501,8 @@ const LandingPage = () => {
               </p>
             </div>
 
-            {/* Quick Links */}
-            <div className="grid grid-cols-2 sm:grid-cols-3 gap-8 md:gap-16">
+            {/* Quick Links - Centered Grid */}
+            <div className="grid grid-cols-2 gap-8 md:gap-16 w-full md:w-auto md:max-w-md justify-center mx-auto md:mx-0">
               {/* Products */}
               <div className="flex flex-col space-y-3">
                 <h3 className="text-white font-semibold mb-2">Products</h3>
@@ -576,7 +575,6 @@ const LandingPage = () => {
                     <path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84" />
                   </svg>
                 </a>
-
                 <a
                   href="https://telegram.com/quotum"
                   target="_blank"
@@ -591,7 +589,6 @@ const LandingPage = () => {
                     <path d="M12 0C5.373 0 0 5.373 0 12s5.373 12 12 12 12-5.373 12-12S18.627 0 12 0zm5.894 8.221l-1.97 9.28c-.145.658-.537.818-1.084.508l-3-2.21-1.446 1.394c-.14.18-.357.223-.554.223l.198-2.8 5.106-4.618c.222-.196-.054-.304-.346-.108L7.83 13.775l-2.718-.816c-.59-.182-.608-.59.124-.873l10.614-4.103c.48-.176.905.114.044.238z" />
                   </svg>
                 </a>
-
                 <a
                   href="https://discord.gg/quotum"
                   target="_blank"
